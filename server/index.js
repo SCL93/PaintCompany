0a0c9f1e735b1c470a3843d966ae909476a28cc7
sungchi <sungchi@students.cs.ubc.ca>
2024-03-25 00:08:21 -0700

    add corsOptions

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const corsOptions = {
 
 const app = express()
 app.use(express.json())
-app.use(cors());
+app.use(cors(corsOptions));
 dotenv.config()
 app.use('/auth', UserRouter)
 app.use('/paint', paintsRouter)
